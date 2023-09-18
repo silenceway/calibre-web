d04747665022386fe000e5ca7e13e270f2f004db
Byron H. <bh@axul.net>
2023-09-17 21:13:45 -0500

    Add 'list pages' link to administration. Sponsor: Fundación Karisma

--- cps/page.py
+++ cps/page.py
@@ -30,8 +30,8 @@ def get_page(file):
 
             return render_title_template('page.html', body=body, title=page.title, page=page.name)
         else:
-            log.error("'%s' was accessed but file doesn't exists." % file)
+            log.debug("'%s' was accessed but file doesn't exists." % file)
             abort(404)
     else:
-        log.error("'%s' was accessed but is not enabled or it's not in database." % file)
+        log.debug("'%s' was accessed but is not enabled or it's not in database." % file)
         abort(404)
